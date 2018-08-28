d4b36bba4d4ce045560774edb404f69ec0ca3717
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2018-08-28 14:35:24 +1200

    adding padding to scripts

--- recvvid.sh
+++ recvvid.sh
@@ -1,2 +1,6 @@
 #/bin/bash
-python -u stdpipe.py /dev/ttyUSB1 1000000 in | pv | gst-launch-1.0  filesrc location=/dev/stdin ! h264parse ! avdec_h264 ! autovideosink sync=False
+### no padding
+#python -u stdpipe.py /dev/ttyUSB1 1000000 in | pv | gst-launch-1.0  filesrc location=/dev/stdin ! h264parse ! avdec_h264 ! autovideosink sync=False
+
+#with padding
+python -u stdpipe2.py /dev/ttyUSB1 1000000 in | gst-launch-1.0  filesrc location=/dev/stdin ! h264parse ! avdec_h264 ! autovideosink sync=False
